e36cbf91888caff5452c8c69a23509da14beaa4e
FBiDev <fernando.birnfeld@gmail.com>
2020-09-18 17:57:18 -0300

    Updated ListItem

--- GNX/Util/ListItem.cs
+++ GNX/Util/ListItem.cs
@@ -6,6 +6,12 @@ namespace GNX
     public class ListItem
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string Name { get; set; }
+
+        public ListItem()
+        {
+            Id = 0;
+            Name = string.Empty;
+        }
     }
 }

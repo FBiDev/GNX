3fe6d62cdca4502bfa3bc9de5393955065579233
FBiDev <fernando.birnfeld@gmail.com>
2020-09-25 17:44:49 -0300

    Added Object.IsNull extension method

---
+++ GNX/Util/ObjectExtesion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+//
+
+namespace GNX
+{
+    public static class ObjectExtesion
+    {
+        public static bool IsNull(this object T)
+        {
+            return T == null;
+        }
+    }
+}

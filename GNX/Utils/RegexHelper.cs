777c469b5a5e9f8e598f23efb0d30e22bff4a0ed
FBiDev <fernando.birnfeld@gmail.com>
2022-05-20 17:08:57 -0300

    Added RegexHelper

---
+++ GNX/Utils/RegexHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//
+using System.Text.RegularExpressions;
+
+namespace GNX
+{
+    public static class RegexHelper
+    {
+        public static string Between(string start, string end, string text, bool singleLine = true)
+        {
+            RegexOptions opt = 0;
+            if (singleLine) { opt = RegexOptions.Singleline; }
+            Regex rg = new Regex(@"" + start + "(.*)" + end + "", opt);
+
+            Match match = rg.Match(text);
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            return string.Empty;
+        }
+    }
+}

b1d75aa172fb2303a4426d9df5ffaa1dd9a4413e
FBiDev <fernando.birnfeld@gmail.com>
2022-05-26 17:45:05 -0300

    Rewrited Regex Class

--- GNX/Generics/cString.cs
+++ GNX/Generics/cString.cs
@@ -6,17 +6,19 @@ using System.Text.RegularExpressions;
 
 namespace GNX
 {
-    public static class RegexHelper
+    public static class cString
     {
-        public static string Between(string start, string end, string text, bool singleLine = true)
+        public static string GetBetween(string start, string end, string text, bool singleLine = true, bool inclusive = false)
         {
             RegexOptions opt = 0;
             if (singleLine) { opt = RegexOptions.Singleline; }
-            Regex rg = new Regex(@"" + start + "(.*)" + end + "", opt);
+            Regex rg = new Regex(@"" + start + "(.*?)" + end + "", opt | RegexOptions.IgnoreCase);
 
             Match match = rg.Match(text);
             if (match.Success)
             {
+                if (inclusive) { return match.Groups[0].Value; }
+
                 return match.Groups[1].Value;
             }
             return string.Empty;

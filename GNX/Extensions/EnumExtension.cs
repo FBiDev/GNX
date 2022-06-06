93a79fb7c13cb7a2f5a36845cd3050de14a6a257
FBiDev <fernando.birnfeld@gmail.com>
2022-06-06 17:57:18 -0300

    Added Enum HexToString

--- GNX/Extensions/EnumExtension.cs
+++ GNX/Extensions/EnumExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Web.UI.WebControls;
 
 namespace GNX
@@ -9,6 +10,25 @@ namespace GNX
         public static string ToSQL(this SortDirection s)
         {
             return s == SortDirection.Ascending ? " ASC " : " DESC ";
+        }
+
+        public static string HexToString(this Enum e)
+        {
+            int i = (int)(object)e;
+            byte[] data = FromHex(i.ToString("X"));
+            string s = Encoding.UTF8.GetString(data);
+            return s;
+        }
+
+        private static byte[] FromHex(string hex)
+        {
+            hex = hex.Replace("-", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
         }
     }
 }

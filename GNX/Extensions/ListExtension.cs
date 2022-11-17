845ba3ca7c2708158762c4eeb3b5f3e36632a4a3
FBiDev <fernando.birnfeld@gmail.com>
2022-11-17 17:55:06 -0300

    Added Move extension to GenericList

--- GNX/Extensions/ListExtension.cs
+++ GNX/Extensions/ListExtension.cs
@@ -24,5 +24,27 @@ namespace GNX
 
             return list[0];
         }
+
+        public static void Move<T>(this List<T> list, T item, int newIndex)
+        {
+            if (item != null)
+            {
+                var oldIndex = list.IndexOf(item);
+                if (oldIndex > -1)
+                {
+                    list.RemoveAt(oldIndex);
+
+                    if (newIndex > oldIndex) newIndex--;
+                    // the actual index could have shifted due to the removal
+
+                    list.Insert(newIndex, item);
+                }
+            }
+        }
+
+        public static void MoveToLast<T>(this List<T> list, T item)
+        {
+            list.Move(item, list.Count);
+        }
     }
 }

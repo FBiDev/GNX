d78ff7813566f78ca547f2dec90e8c55f47e0939
FBiDev <fernando.birnfeld@gmail.com>
2022-11-22 17:58:04 -0300

    Updated List Extensions

--- GNX/Extensions/ListExtension.cs
+++ GNX/Extensions/ListExtension.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+//
+using System.Text.RegularExpressions;
 
 namespace GNX
 {
@@ -45,6 +48,32 @@ namespace GNX
         public static void MoveToLast<T>(this List<T> list, T item)
         {
             list.Move(item, list.Count);
+        }
+
+        public static void MoveToLast<T>(this List<T> list, IEnumerable<T> items)
+        {
+            items.ToList().ForEach(x => list.Move(x, list.Count));
+        }
+
+        public static void MoveToFirst<T>(this List<T> list, T item)
+        {
+            list.Move(item, 0);
+        }
+
+        public static void MoveToFirst<T>(this List<T> list, IEnumerable<T> items)
+        {
+            items.ToList().ForEach(x => list.Move(x, 0));
+        }
+
+        public static IEnumerable<T> OrderByNatural<T>(this IEnumerable<T> items, Func<T, string> selector, StringComparer stringComparer = null)
+        {
+            var regex = new Regex(@"\d+", RegexOptions.Compiled);
+
+            int maxDigits = items
+                          .SelectMany(i => regex.Matches(selector(i)).Cast<Match>().Select(digitChunk => (int?)digitChunk.Value.Length))
+                          .Max() ?? 0;
+
+            return items.OrderBy(i => regex.Replace(selector(i), match => match.Value.PadLeft(maxDigits, '0')), stringComparer ?? StringComparer.CurrentCulture);
         }
     }
 }

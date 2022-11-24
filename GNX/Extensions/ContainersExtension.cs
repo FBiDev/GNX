be3b3ffd04108b3875a9ffb11648f6e495cec8a9
FBiDev <fernando.birnfeld@gmail.com>
2022-11-24 14:03:18 -0300

    Added GridColumn styles extensions

--- GNX/Extensions/ContainersExtension.cs
+++ GNX/Extensions/ContainersExtension.cs
@@ -8,6 +8,63 @@ namespace GNX
 {
     public static class ContainersExtension
     {
+        //Columns
+        public static void StyleCenter(this DataGridViewColumnCollection source, params int[] cols)
+        {
+            var style = new DataGridViewCellStyle()
+            {
+                Alignment = DataGridViewContentAlignment.MiddleCenter
+            };
+
+            foreach (var c in cols)
+                source[c].DefaultCellStyle = style;
+        }
+
+        public static void StyleNumber(this DataGridViewColumnCollection source, params int[] cols)
+        {
+            var style = new DataGridViewCellStyle()
+            {
+                Alignment = DataGridViewContentAlignment.MiddleRight,
+                Format = "N0",
+                NullValue = null
+            };
+
+            foreach (var c in cols)
+            {
+                //source[c].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                source[c].DefaultCellStyle = style;
+            }
+        }
+
+        public static void StyleNumberCenter(this DataGridViewColumnCollection source, params int[] cols)
+        {
+            foreach (var c in cols)
+            {
+                StyleNumber(source, c);
+                StyleCenter(source, c);
+            }
+        }
+
+        public static void StyleImage(this DataGridViewColumnCollection source, params int[] cols)
+        {
+            var style = new DataGridViewCellStyle()
+            {
+                Alignment = DataGridViewContentAlignment.MiddleCenter,
+                Padding = new System.Windows.Forms.Padding(2)
+            };
+
+            foreach (var c in cols)
+            {
+                if (source[c] is DataGridViewImageColumn)
+                {
+                    source[c].DefaultCellStyle = style;
+                    source[c].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                    ((DataGridViewImageColumn)source[c]).ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+                }
+            }
+        }
+
+        //Rows
         public static bool Empty(this DataGridViewRowCollection source)
         {
             return source.Count == 0;
@@ -23,6 +80,7 @@ namespace GNX
             return source.Count > 0;
         }
 
+        //Cells
         public static bool RowHeader(this DataGridViewCellEventArgs e)
         {
             return e.RowIndex == -1;

5006e46ea7fedca14fc5c26170ac797a222aca39
FBiDev <fernando.birnfeld@gmail.com>
2024-02-01 18:19:49 -0300

    Update: ListItem

--- GNX/Any/Generics/ListItem.cs
+++ GNX/Any/Generics/ListItem.cs
@@ -11,6 +11,12 @@
             Text = string.Empty;
         }
 
+        public ListItem(int value, string text)
+        {
+            Value = value;
+            Text = text;
+        }
+
         public override string ToString()
         {
             return Text + " - " + Value.ToString();
